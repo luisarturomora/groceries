f8caa48c3d44a03d47766ba8db8c37af16a9754b
LUIS-PC\Luis <luisarturomm12@gmail.com>
2017-06-19 11:59:18 -0400

    First Commit

---
+++ app/pages/login/login.component.ts
@@ -0,0 +1,72 @@
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
+import { Page } from 'ui/page';
+import { Color } from 'color';
+import { View } from 'ui/core/view';
+
+import { User } from '../../shared/user/user';
+import { UserService } from "../../shared/user/user.service";
+
+@Component({
+  selector: "my-app",
+  providers: [UserService],
+  templateUrl: './pages/login/login.html',
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
+})
+
+export class LoginComponent implements OnInit {
+  user: User;
+  isLoggingIn = true;
+  @ViewChild('container') container : ElementRef;
+
+  ngOnInit() {
+    this.page.actionBarHidden = true;
+    this.page.backgroundImage = "res://bg_login";
+  } 
+
+  constructor(private router: Router, private userService : UserService, private page : Page){
+    this.user = new User();
+    this.user.email = 'luis@gmail.com';
+    this.user.password = 'luis';
+  }
+
+  submit(){
+    if(!this.user.isValidEmail()){
+      alert('Enter a valid email');
+      return;
+    }
+    if(this.isLoggingIn){
+      this.login();
+    } else {
+      this.signUp();
+    }
+  }
+
+  toggleDisplay() {
+    this.isLoggingIn = !this.isLoggingIn;
+    let container = <View>this.container.nativeElement;
+    container.animate({
+      backgroundColor: this.isLoggingIn ? new Color("white") : new Color("#301217"),
+      duration: 200
+    });
+}
+
+    login() {
+      this.userService.login(this.user)
+        .subscribe(
+          () => this.router.navigate(["/list"]),
+          (error) => alert("Unfortunately we could not find your account.")
+          );
+    }
+
+  signUp() {
+    this.userService.register(this.user)
+      .subscribe(
+        () => {
+          alert("Your account was successfully created.");
+          this.toggleDisplay();
+        },
+        () => alert("Unfortunately we were unable to create your account.")
+      );
+  }
+}
